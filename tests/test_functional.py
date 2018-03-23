7d355149da3e68ed23e5a1df9da394dce067b520
Azul <azul@riseup.net>
2018-03-23 13:23:16 +0100

    claims: verify the inclusion of ones own key
    This only works on the third message exchanged though:
    1) send AC header now peer has OpenPGP key
    2) receive CC header now I have peers CC key
    3) send message with claim readable to peer in my own CC

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -6,6 +6,7 @@ import os
 from test_muacrypt.test_account import gen_ac_mail_msg
 from muacryptcc.plugin import CCAccount
 from muacryptcc.filestore import FileStore
+from claimchain.utils import bytes2ascii
 
 
 def test_no_claim_headers_in_cleartext_mail(account_maker):
@@ -18,12 +19,28 @@ def test_no_claim_headers_in_cleartext_mail(account_maker):
 
 def test_claim_headers_in_encrypted_mail(account_maker):
     acc1, acc2 = account_maker(), account_maker()
-
     send_and_process_mail(acc1, acc2)
-    dec_msg = send_and_process_encrypted_mail(acc2, acc1).dec_msg
+
+    dec_msg = send_and_process_encrypted_mail(acc2, acc1)[1].dec_msg
     account = get_cc_account(dec_msg['ChainStore'])
     assert dec_msg['GossipClaims'] == account.head_imprint
     assert account.has_readable_claim(acc1.addr)
+
+
+def test_claim_headers_in_encrypted_mail(account_maker):
+    acc1, acc2 = account_maker(), account_maker()
+    send_and_process_mail(acc1, acc2)
+
+    r, mail = send_and_process_encrypted_mail(acc2, acc1)
+    cc2 = get_cc_account(mail.dec_msg['ChainStore'])
+    ac2 = r.pah
+
+    r, mail = send_and_process_encrypted_mail(acc1, acc2)
+    cc1 = get_cc_account(mail.dec_msg['ChainStore'])
+    ac1 = r.pah
+
+    assert mail.dec_msg['GossipClaims'] == cc1.head_imprint
+    assert cc1.read_claim_as(cc2, acc2.addr) == bytes2ascii(ac2.keydata)
 
 
 # send a mail from acc1 with autocrypt key to acc2
@@ -40,8 +57,7 @@ def send_and_process_encrypted_mail(acc1, acc2):
     """
     msg = gen_ac_mail_msg(acc1, acc2, payload="hello ä umlaut", charset="utf8")
     enc_msg = acc1.encrypt_mime(msg, [acc2.addr]).enc_msg
-    acc2.process_incoming(enc_msg)
-    return acc2.decrypt_mime(enc_msg)
+    return acc2.process_incoming(enc_msg), acc2.decrypt_mime(enc_msg)
 
 
 def get_cc_account(store_dir):
